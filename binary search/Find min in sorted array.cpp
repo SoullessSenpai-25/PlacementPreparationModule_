339540faccaef239b49b89fc666a3f8307101349
Prakhar Shukla <132089601+SoullessSenpai-25@users.noreply.github.com>
2023-08-05 17:38:36 +0530

    Create Find min in sorted array.cpp

---
+++ binary search/Find min in sorted array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+
+public:
+    int findMin(vector<int>& nums) {
+        int ans=INT_MAX;
+        int l=0;int r=nums.size()-1;
+      ;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[l]<=nums[r]){
+                ans=min(ans,nums[l]);
+                break;
+            }
+            if(nums[l]<=nums[mid]){
+                ans=min(ans,nums[l]);
+                l=mid+1;
+            }
+            else{
+                ans=min(ans,nums[l]);
+                r=mid-1;
+
+            }
+        }return ans;
+
+
+        
+    }
+};
